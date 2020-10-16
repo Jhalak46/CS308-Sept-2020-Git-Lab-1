482b2e90c311eb145c19d708ac69d749f99ccecc
Jhalak Choudhary <jhalak046@gmail.com>
2020-10-17 02:05:53 +0530

    added NEGSUPPORT: to all printfs

--- sqrt.c
+++ sqrt.c
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 {
 	if (argc != 2) 
 	{
-		printf("DEBUG:Sqrt of %d is %f i\n", input, sqrt(abs(input)));
+		printf("NEGSUPPORT:Sqrt of %d is %f i\n", input, sqrt(abs(input)));
 		return(0);
 	}
 	if(input < 0) 
