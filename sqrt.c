a34aed8ded47c6f4ec91b68bfd9ff4c510e2189f
Jhalak Choudhary <jhalak046@gmail.com>
2020-10-17 02:11:58 +0530

    print DEBUG: final commit

--- sqrt.c
+++ sqrt.c
@@ -10,11 +10,13 @@ positive number. */
 
 int main(int argc, char* argv[]) 
 {
-	if (argc != 2) {
+	if (argc != 2) 
+	{
 		printf("DEBUG:Sqrt of %d is %f i\n", input, sqrt(abs(input)));
 		return(0);
 	}
-	if(input < 0) {
+	if(input < 0) 
+	{
 		printf("DEBUG:Invalid Input");
 		exit(-1);
 	}
