4672903ea680e4385548fea766ad0e951a475ff7
Jhalak Choudhary <jhalak046@gmail.com>
2020-10-17 01:00:56 +0530

    Check if Number is valid

--- sqrt.c
+++ sqrt.c
@@ -8,14 +8,18 @@ positive number. */
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char* argv[]) {
-	
-	if (argc != 2) {
+int main(int argc, char* argv[]) 
+{
+	if (argc != 2) 
+	{
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
-	
-
+	if(input < 0) 
+	{
+		printf("Invalid Input");
+		exit(-1);
+	}
 	int input = atoi(argv[1]);
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting.");
